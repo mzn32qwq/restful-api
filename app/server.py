e88dc1c0ba78c777678f0cf7476e096bf1222b51
jingjing.xiong <hazelxiong21@gmail.com>
2024-02-28 20:27:35 +0100

    authentication server source code for build docker image

--- app/server.py
+++ app/server.py
@@ -1,3 +1,6 @@
+import json
+# import os
+
 from flask import Flask, request, jsonify
 import hashlib
 import JWT
@@ -5,7 +8,21 @@ from datetime import datetime, timedelta
 
 app = Flask(__name__)
 
-users = {}
+# USER_SHORT_URL_FILE = os.getenv('USER_SHORT_URL_FILE')
+USER_SHORT_URL_FILE = "/auth/file/url_short.json"
+
+def load_users():
+    try:
+        with open(USER_SHORT_URL_FILE, 'r+') as file:
+            return json.load(file)
+    except FileNotFoundError:
+        return {}
+
+def save_users(users):
+    with open(USER_SHORT_URL_FILE, 'w') as file:
+        json.dump(users, file)
+
+users = load_users()
 
 def hash_password(password):
     """hash password with sha256"""
@@ -14,37 +31,43 @@ def hash_password(password):
 @app.route('/users', methods=['POST'])
 def create_user():
     global users
+    users = load_users()
     data = request.json
     username = data.get('username')
     password = data.get('password')
 
     if username in users:
         return jsonify({'detail': "duplicate"}), 409
-
-    users[username] = hash_password(password)
+    users[username] = {'password': hash_password(password)}
+    save_users(users)
     return jsonify({'message': 'User created'}), 201
 
 @app.route('/users', methods=['PUT'])
 def change_password():
     global users
+    users = load_users()
     data = request.json
     username = data.get('username')
     old_password = data.get('password')
     new_password = data.get('new_password')
 
-    if username not in users or users[username] != hash_password(old_password):
+    if username not in users or users[username]['password'] != hash_password(old_password):
         return jsonify({'detail':"forbidden" }), 403
 
-    users[username] = hash_password(new_password)
+    users[username]['password'] = hash_password(new_password)
+    save_users(users)
     return jsonify({'message': 'Password changed'}), 200
 
 @app.route('/users/login', methods=['POST'])
 def login():
+    global users
+    users = load_users()
+
     data = request.json
     username = data.get('username')
     password = data.get('password')
 
-    if username in users and users[username] == hash_password(password):
+    if username in users and users[username]['password'] == hash_password(password):
         payload = {
     "sub": password ,
     "name": username,
@@ -55,17 +78,7 @@ def login():
         return jsonify({'token': token}), 200
 
     return jsonify({'detail': "forbidden"}), 403
-@app.route('/check', methods=['PUT'])
-def check():
-    data = request.json
-    token = data.get('token')
-    if not JWT.verify_jwt(token):
-        return jsonify({"status": False, "user": "Unauthorized"})
-    checkusername = JWT.return_username(token)
-    if checkusername not in users:
-        return jsonify({"status": False, "user": "User not found"})
-    return jsonify({"status": True, "user": checkusername})
+
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5001)
-
+    app.run(debug=True, port=5001,host="0.0.0.0")
